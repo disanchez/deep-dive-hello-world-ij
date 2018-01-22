b719f779a9b1cafc7ebb9e2da34e0a529e44bf48
Donny <donnysanchez@msn.com>
2018-01-21 21:37:47 -0700

    Add salute method and test for command line arguments.

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.cnm.deepdive.prework;
 public class HelloWorld {
 
   public static void main(String[]  args)  {
-    salute((args.length > 0) ? args [0] : "World");
+    salute((args.length > 0) ? args [0] : "World"); 
   }
 
   private static void salute(String target)  {
